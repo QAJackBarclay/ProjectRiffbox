762be985daaaf54080d5d3c26436e070c96a79b9
QAJackBarclay <jackbarclay@live.com>
2022-05-31 09:57:07 +0000

    Third push to save

--- application/forms.py
+++ application/forms.py
@@ -2,6 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, ValidationError
 
+class Register_1(FlaskForm):
+    first_name = StringField('What is your name?')
+    submit = SubmitField('Enter site')
+
+
 class BasicForm(FlaskForm):
     name = StringField("Your Name: ", validators=[DataRequired()])
     favourite_1 = SelectField("Pick a genre: ", choices = ['Rock', 'Metal', 'Reggae', 'Disco', 'Hardstyle' ])
--- application/routes.py
+++ application/routes.py
@@ -1,13 +1,30 @@
 from application import app
-from flask import render_template
-from application.forms import BasicForm
+from flask import render_template, request
+from application.forms import BasicForm, Register_1
 
 
 @app.route ('/')
 
-@app.route('/home')
-def home():
-     return render_template('home.html')
+#@app.route('/home')
+#def home():
+#return render_template('home.html', form=form, message=message)
+
+@app.route('/home', methods=['GET', 'POST'])
+def register_1():
+     message = "Enter name: "
+     form = Register_1()
+     
+     if request.method == 'POST':
+          first_name = form.first_name.data
+
+          if len(first_name) == 0:
+               message = "Please supply your name!"
+          else:
+               message = f'Thank you, {first_name}'
+               return render_template('home.html', form=form, message=message)
+
+     return render_template('home.html', form=form, message=message)
+
 
 @app.route('/index')
 def index():
@@ -16,6 +33,7 @@ def index():
 @app.route('/instructions')
 def instructions():
      return render_template('instructions.html')
+
 
 @app.route('/result')
 def result():
