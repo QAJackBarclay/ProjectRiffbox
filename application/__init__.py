e29fb126df9230a4390316c2ce96419e4981e0ef
QAJackBarclay <jackbarclay@live.com>
2022-06-17 10:17:05 +0000

    database and key updated

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import pdb
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 from application import routes
 
+
+#'sqlite:///db.sqlite'
+#'YOUR_SECRET_KEY'
